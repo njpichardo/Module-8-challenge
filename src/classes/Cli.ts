5afd365981c2d6be1a2bd9bab7b7fc17e007f12b
njpichardo <njpichardo@gmail.com>
2024-12-15 21:43:13 -0500

    updated Cli.ts, created index.js

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -8,16 +8,14 @@ import Wheel from "./Wheel.js";
 // define the Cli class
 class Cli {
   // TODO: update the vehicles property to accept Truck and Motorbike objects as well
-  // TODO: You will need to use the Union operator to define additional types for the array
-  // TODO: See the AbleToTow interface for an example of how to use the Union operator
-  vehicles: (Car)[];
+  vehicles: (Car | Truck | Motorbike)[];
   selectedVehicleVin: string | undefined;
   exit: boolean = false;
 
-  // TODO: Update the constructor to accept Truck and Motorbike objects as well
-  constructor(vehicles: (Car)[]) {
+  constructor(vehicles: (Car | Truck | Motorbike)[]) {
     this.vehicles = vehicles;
   }
+
 
   // static method to generate a vin
   static generateVin(): string {
